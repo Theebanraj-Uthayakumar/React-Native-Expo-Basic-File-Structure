5ef4d5df45fbc55b115b91f96bd9349e47987b12
Theeban <69495468+Theebanraj-Uthayakumar@users.noreply.github.com>
2021-10-22 02:27:57 +0530

    Basic Structure

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <Text style={styles.text}>HomeScreen This is Theebanraj Uthayakumar</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+  },
+});
+
+export default HomeScreen;
